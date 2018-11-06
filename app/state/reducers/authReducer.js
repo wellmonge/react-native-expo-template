f27400bae99bf6e50f3b6c71486380c63d6fe604
wellinton monge <mongewellinton@gmail.com>
2018-11-05 22:17:38 -0200

    ..setting up redux

---
+++ app/state/reducers/authReducer.js
@@ -0,0 +1,16 @@
+import { AUTH_TO_SERVER } from "../types/authType";
+
+const initialState = {
+
+}
+
+export default authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case AUTH_TO_SERVER:
+        
+            break;
+    
+        default:
+            return state;
+    }
+}
---
+++ app/state/types/authType.js
@@ -0,0 +1,7 @@
+/*
+ * action types
+ */
+
+export const AUTH_TO_SERVER = 'AUTH_TO_SERVER'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
